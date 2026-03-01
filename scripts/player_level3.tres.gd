aa0b5cdf85ef192da5e43997844c16c1f507e93a
Basil <madisonsa023@gmail.com>
2026-03-01 10:41:31 -0500

    GAME IS DONE

--- scripts/player_level3.tres.gd
+++ scripts/player_level3.tres.gd
@@ -35,4 +35,4 @@ func _on_kill_zone_bottom_end_body_entered(body: Node2D) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://midnight.tscn")
+	get_tree().change_scene_to_file("res://end_scene.tscn")
