a4a38e224b18fddaf7c6d1342d27c00177d11f4b
Basil <madisonsa023@gmail.com>
2026-03-02 10:45:41 -0500

    fixed the net hitboxes

--- scripts/player_level3.tres.gd
+++ scripts/player_level3.tres.gd
@@ -36,3 +36,7 @@ func _on_kill_zone_bottom_end_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://end_scene.tscn")
+
+
+func _on_kill_zone_nets_body_entered(body: Node2D) -> void:
+	killPlayer()
